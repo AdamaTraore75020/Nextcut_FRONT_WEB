078feb99db8f3d4593491f13711a1229efc2e873
atraore <atraore@sodifrance.fr>
2020-02-11 17:44:22 +0100

    setup firebase on project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,22 @@ import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAcySmggNJfe9J9TvYvkzB_A1xsvnRqzKQ",
+  authDomain: "nextcutdev.firebaseapp.com",
+  databaseURL: "https://nextcutdev.firebaseio.com",
+  projectId: "nextcutdev",
+  storageBucket: "nextcutdev.appspot.com",
+  messagingSenderId: "64410353141",
+  appId: "1:64410353141:web:23f66c077fd8255391f4bc"
+};
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +28,12 @@ import { LoginComponent } from './login/login.component';
     LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // 3. Initialize firebase
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule // storage
   ],
   providers: [],
   bootstrap: [AppComponent]
